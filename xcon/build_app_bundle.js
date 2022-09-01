81c6914fe5865b198c6c1aa2bd00dc7fcd612877
malikkurosaki <kurosakiblackangel@gmail.com>
2022-09-01 13:01:31 +0800

    Thu Sep  1 13:01:31 WITA 2022

--- xcon/build_app_bundle.js
+++ xcon/build_app_bundle.js
@@ -6,5 +6,6 @@ const server = path.join(__dirname, "./mode_pro.js");
 const gitPush = path.join(__dirname, "./git_push.js");
 exec(`node ${client}`, { stdio: "inherit" });
 exec(`node ${server}`, { stdio: "inherit" });
+
 exec(`flutter build appbundle`, { stdio: "inherit", cwd: path.join(__dirname, "../") });
 exec(`node ${gitPush}`, { stdio: "inherit" });
--- xcon/git_push.js
+++ xcon/git_push.js
@@ -2,4 +2,9 @@
 const exec = require('child_process').execSync
 const path = require('path');
 
+
+require('./mode_client_pro');
+require('./mode_pro');
+
 exec(`git add . && git commit -m "$(date)" && git push origin main`, { stdio: "inherit", cwd: path.join(__dirname, "./../") })
+console.log("push finis");
